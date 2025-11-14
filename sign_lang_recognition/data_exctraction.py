ceebcf3a7cc9e76f87e84b3630685ad955ec5afb
Vukasin32 <vuletasine@gmail.com>
2025-11-14 11:52:06 +0100

    Update data_exctraction.py

--- sign_lang_recognition/data_exctraction.py
+++ sign_lang_recognition/data_exctraction.py
@@ -4,9 +4,9 @@ import cv2
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3) # This object is used to detect hands
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3) # Objekat koji se koristi za detekciju
 
-directory = './data' # Use data from new directory if you made your own base
+directory = './data' # Direktorijum u kom se nalaze slike
 data = []
 labels = []
 for class_dir in os.listdir(directory):
@@ -16,7 +16,7 @@ for class_dir in os.listdir(directory):
 
         data_acc = []
         result = hands.process(img_rgb)
-        if result.multi_hand_landmarks and len(result.multi_hand_landmarks) == 1: # Only one hand can be shown as sign, e.g. the letter
+        if result.multi_hand_landmarks and len(result.multi_hand_landmarks) == 1: # Mora da bude detektovana jedna šaka
             for hand_landmarks in result.multi_hand_landmarks:
                 for i in range(len(hand_landmarks.landmark)):
                     x = hand_landmarks.landmark[i].x
@@ -24,7 +24,7 @@ for class_dir in os.listdir(directory):
                     data_acc.append(x)
                     data_acc.append(y)
 
-            if len(data_acc) == 42: # Data will be passed only if all landmarks can be registered in image
+            if len(data_acc) == 42: # Podatak se smatra validnim samo ukoliku su sve značajne tačke šake detektovane
                 data.append(data_acc)
                 labels.append(class_dir)
 
